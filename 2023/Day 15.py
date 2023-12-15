c5d718a364e294bc47b620e7eb6f0475e0e756d4
The Khang Dang <tk.dang@tum.de>
2023-12-15 10:49:19 +0100

    Cleaned up day 15

--- 2023/Day 15.py
+++ 2023/Day 15.py
@@ -1,4 +1,5 @@
 from utils.data import *
+from functools import reduce
 
 
 def parse(data):
@@ -6,10 +7,7 @@ def parse(data):
 
 
 def hash(sequence):
-    res = 0
-    for c in sequence:
-        res = ((res + ord(c)) * 17) % 256
-    return res
+    return reduce(lambda i, c: (i + ord(c)) * 17 % 256, sequence, 0)
 
 
 def part1(sequences):
@@ -19,32 +17,20 @@ def part1(sequences):
 def calculate_power(boxes):
     total = 0
     for i, box in enumerate(boxes, 1):
-        for j, (_, power) in enumerate(box, 1):
+        for j, power in enumerate(box.values(), 1):
             total += i * j * power
     return total
 
 
 def part2(sequences):
-    boxes = [[] for _ in range(256)]
-    pos = {}
-    for seq in sequences:
-        if "=" in seq:
-            label, strength = seq.split("=")
-            if label in pos:
-                box, idx = pos[label]
-                boxes[box][idx][1] = int(strength)
-            else:
-                box = hash(label)
-                pos[label] = (box, len(boxes[box]))
-                boxes[box].append([label, int(strength)])
-        else:
-            label = seq[:-1]
-            if label in pos:
-                box, idx = pos[label]
-                del boxes[box][idx]
-                del pos[label]
-                for i, (lab, _) in enumerate(boxes[box]):
-                    pos[lab] = (box, i)
+    boxes = [dict() for _ in range(256)]
+
+    for step in sequences:
+        match step.strip('-').split('='):
+            case [label, strength]:
+                boxes[hash(label)][label] = int(strength)
+            case [label]:
+                boxes[hash(label)].pop(label, 0)
     return calculate_power(boxes)
 
 
